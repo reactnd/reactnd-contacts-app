ecaefb2b419db34b0eb79f37a781aca12574cb0d
Ruargh <ruargh@gmail.com>
2018-12-07 09:09:00 +0200

    Update state with setState - revert to course git repo

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React, { Component } from 'react';
-import ListContacts from './ListContacts';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
 
 class App extends Component {
   state = {
     contacts: [
       {
+        id: 'tyler',
+        name: 'Tyler McGinnis',
+        handle: '@tylermcginnis',
+        avatarURL: 'http://localhost:5001/tyler.jpg'
+      },
+      {
         id: 'karen',
         name: 'Karen Isgrigg',
-        handle: 'karen_isgrigg',
+        handle: '@karen_isgrigg',
         avatarURL: 'http://localhost:5001/karen.jpg'
       },
       {
         id: 'richard',
         name: 'Richard Kalehoff',
-        handle: 'richardkalehoff',
+        handle: '@richardkalehoff',
         avatarURL: 'http://localhost:5001/richard.jpg'
       },
-      {
-        id: 'tyler',
-        name: 'Tyler McGinnis',
-        handle: 'tylermcginnis',
-        avatarURL: 'http://localhost:5001/tyler.jpg'
-      }
     ]
-  };
-  removeContact = contact => {
-    this.setState(currentState => ({
-      contacts: currentState.contacts.filter(c => {
-        return c.id !== contact.id;
+  }
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
       })
-    }));
-  };
+    }))
+  }
   render() {
     return (
       <div>
@@ -39,8 +39,8 @@ class App extends Component {
           onDeleteContact={this.removeContact}
         />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React from 'react'
 
-const ListContacts = ({contacts}) => {
-
+function ListContacts (props) {
   return (
-    <ol className="ordered-list">
-    {contacts.map(contact => (
-      <li key={contact.id} className="contact-list-item">
-        <div
-          className="contact-avatar"
-          style={{
-            backgroundImage: `url(${contact.avatarURL})`
-          }}
-        />
-        <div className="contact-details">
-          <p>{contact.name}</p>
-          <p>{contact.handle}</p>
-        </div>
-        <button className="contact-remove">Remove</button>
-      </li>
-    ))}
-  </ol>
+    <ol className='contact-list'>
+      {props.contacts.map((contact) => (
+        <li key={contact.id} className='contact-list-item'>
+          <div
+            className='contact-avatar'
+            style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}
+          ></div>
+          <div className='contact-details'>
+            <p>{contact.name}</p>
+            <p>{contact.handle}</p>
+          </div>
+          <button
+            onClick={() => props.onDeleteContact(contact)}
+            className='contact-remove'>
+              Remove
+          </button>
+        </li>
+      ))}
+    </ol>
   )
 }
 
-export default ListContacts;
+export default ListContacts
