6cd177066146c26534e2434f0a1d5b602f9a9937
Ruargh <ruargh@gmail.com>
2018-12-07 08:48:08 +0200

    listcontacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ListContacts extends Component {
-  render() {
-    console.log('Props', this.props);
-    return (
-      <div>
-        <ol className="ordered-list">
-          {this.props.contacts.map(contact => (
-            <li key={contact.id} className="contact-list-item">
-              <div
-                className="contact-avatar"
-                style={{
-                  backgroundImage: `url(${contact.avatarURL})`
-                }}
-              />
-              <div className="contact-details">
-                <p>{contact.name}</p>
-                <p>{contact.handle}</p>
-              </div>
-              <button className="contact-remove">Remove</button>
-            </li>
-          ))}
-        </ol>
-      </div>
-    );
-  }
+const ListContacts = ({contacts}) => {
+
+  return (
+    <ol className="ordered-list">
+    {contacts.map(contact => (
+      <li key={contact.id} className="contact-list-item">
+        <div
+          className="contact-avatar"
+          style={{
+            backgroundImage: `url(${contact.avatarURL})`
+          }}
+        />
+        <div className="contact-details">
+          <p>{contact.name}</p>
+          <p>{contact.handle}</p>
+        </div>
+        <button className="contact-remove">Remove</button>
+      </li>
+    ))}
+  </ol>
+  )
 }
 
 export default ListContacts;
