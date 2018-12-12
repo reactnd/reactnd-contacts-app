be0df8fc18062c07428a53264ae3d0189678cf01
Ruargh <ruargh@gmail.com>
2018-12-12 12:36:58 +0200

    Lesson 5: React Router | Dynamically Render Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact'
 
 class App extends Component {
   state = {
-    contacts: []
+    contacts: [],
+    screen: 'list'
   };
 
   componentDidMount() {
@@ -45,10 +47,21 @@ class App extends Component {
   render() {
     return (
       <div>
+        {this.state.screen === 'list' &&(
         <ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-        />
+        onNavigate={() => {
+          this.setState(()=> ({
+            screen: 'create'
+          }))
+        }}
+        contacts={this.state.contacts}
+        onDeleteContact={this.removeContact}
+      />
+        )}
+        {this.state.screen === 'create' && (
+        <CreateContact />
+        )}
+
       </div>
     );
   }
