e0aba16330596a5cbeea2f820a59298c1f07f37a
Ruargh <ruargh@gmail.com>
2018-12-18 13:02:59 +0200

    Lesson 5 - React Router | 5. The Route Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 import * as ContactsAPI from './utils/ContactsAPI';
-import CreateContact from './CreateContact'
+import CreateContact from './CreateContact';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   state = {
     contacts: [],
-    screen: 'list'
   };
 
   componentDidMount() {
@@ -47,20 +47,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.screen === 'list' &&(
-        <ListContacts
-        onNavigate={() => {
-          this.setState(()=> ({
-            screen: 'create'
-          }))
-        }}
-        contacts={this.state.contacts}
-        onDeleteContact={this.removeContact}
-      />
-        )}
-        {this.state.screen === 'create' && (
-        <CreateContact />
-        )}
+        <Route exact
+          path="/"
+          render={() => (
+            <ListContacts
+              contacts={this.state.contacts}
+              onDeleteContact={this.removeContact}
+            />
+          )}
+        />
+        <Route path='/create' component={CreateContact} />
 
       </div>
     );
